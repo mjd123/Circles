223b1c23d124ec3e043218770290bb2ba2df6f4e
Marcus Denny <mj88denny@googlemail.com>
2016-10-19 15:38:08 +0100

    Add files via upload
    Added some css and js to fix bugs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+var circle = document.querySelector('#circle');
+var newDiv = document.createElement("div")
+var div = document.querySelector("div")
+var content = document.querySelector('#content')
+var clickCount = 0;
 
-var circleNav = document.getElementById('circle')
-document.body.appendChild(menu);
+	circle.onclick= function () {
+
+		newDiv.setAttribute('class', 'myclass');
+		content.appendChild(newDiv);
+		content.parentNode.insertBefore(newDiv, content);
+		newDiv.innerHTML = "Alloquendo <p> conjunctam</p>";
+		console.log("click");
+		clickCount ++
+	}
+
+			if(clickCount < 2){
+				newDiv.onclick = function() {
+
+					newDiv.parentNode.removeChild(newDiv)
+						console.log("removed")
+								}
+											}
