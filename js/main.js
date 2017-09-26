87ce4960c90af35dc71345f17108b187495ec113
Marcus <mj88denny@googlemail.com>
2017-09-26 01:39:42 +0100

    CSS changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 
-var circleNav = document.getElementById('circle')
-document.body.appendChild(menu);
+const content = document.body.querySelector('.content');
+const lineLeft = document.createElement('div');
+content.appendChild(lineLeft);
+lineLeft.classList.add('line_Left');
+
+const lineRight = document.createElement('div');
+content.appendChild(lineRight);
+lineRight.classList.add('line_Right');
+
+function lineWalk() {
+  lineRight.classList.add('line_Right_Active');
+  lineLeft.classList.add('line_Left_Active');
+}
+
+window.addEventListener('load', lineWalk);
