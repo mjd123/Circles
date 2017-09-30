90abd705df5178308adf224eb898acf7535196fa
mjd123 <mj88denny@googlemail.com>
2017-09-30 20:13:56 +0100

    circle functions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 const content = document.body.querySelector('.content');
 const lineLeft = document.createElement('div');
 content.appendChild(lineLeft);
@@ -25,39 +24,82 @@ const canvas = document.querySelector("#canvas");
 canvas.width = content.clientWidth;
 canvas.height = content.clientHeight;
 const ctx = canvas.getContext("2d");
+console.log(content.clientWidth);
 
-ctx.lineJoin = "round";
-ctx.lineCap = 'round';
-ctx.lineWidth = 5;
+const circle = document.querySelectorAll('.circle');
+
+//ctx.lineJoin = "round";
+//ctx.lineCap = 'round';
+//ctx.lineWidth = 5;
 
 let particles = [];
 
 var clickX = 0;
 var clickY = 0;
 
-function draw(e){
+function draw(e) {
   particles.push({
     x: e.clientX,
     y: e.clientY,
     radius: getRandomInt(1, 10),
-    opacity: Math.random()* 0.5 + 0,
-    number: 5,
-    distance: getRandomInt(0, 100)
+    opacity: Math.random() * 1 + 0,
+    drawn: false
   });
 
   for (var i = 0; i < particles.length; i++) {
     ctx.beginPath();
-     ctx.globalAlpha = particles[i].opacity;
-     ctx.arc(
-       particles[particles.length / 1.05].x, particles[particles.length / 1.05].y, particles[particles.length / 1.05].radius,
-       false, Math.PI * 2, false);
-       ctx.globalAlpha = particles[i].opacity;
-     ctx.fill();
+    ctx.globalAlpha = particles[i].opacity;
+    ctx.arc(
+      particles[i].x, particles[i].y, particles[i].radius,
+      false, Math.PI * 2, false);
+    ctx.globalAlpha = particles[i].opacity;
+    ctx.fill();
+    particles[i].drawn  = true;
+    console.log(particles[i].y);
 
+  }
 
 }
 
+function circleSuround(e) {
+  var rect = this.getBoundingClientRect();
+  console.log(rect);
+  var position = {
+    top: rect.top + window.pageYOffset,
+    left: rect.left + window.pageXOffset,
+    width: rect.width / 2 + rect.width,
+    height: rect.height / 2 + rect.height
+
+  };
+  //x.x = position.left * Math.random()
+  //x.y = position.top * Math.random()
+
+
+
+
+
+  for (var i = 0; i < particles.length; i++) {
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.globalAlpha = particles[i].opacity;
+    ctx.arc(
+      position.width, position.height, particles[i].radius,
+      false, Math.PI * 2, false);
+    ctx.globalAlpha = particles[i].opacity;
+    ctx.fill();
+    clearCircles()
+  }
+  console.log(position.top, position.left);
+}
+
+function clearCircles() {
+  for (var i = 0; i < particles.length; i++) {
+    if (particles[i].drawn) {
+      particles[i].opacity - 1
+    }
+  }
 
 }
 
 canvas.addEventListener('mousemove', draw);
+circle.forEach((x) => x.addEventListener('mouseover', circleSuround));
